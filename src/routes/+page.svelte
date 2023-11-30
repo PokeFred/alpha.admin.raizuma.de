4d3f4958bcaebae3e67a910b573f34200b92d2e2
PokeFred <cedric.paelmke@gmx.de>
2023-11-30 18:00:49 +0100

    svelte-awesome

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
-<div>Hey</div>
+<script lang="ts">
+    import Icon from "svelte-awesome"
+    import {faThumbsUp} from "@fortawesome/free-solid-svg-icons/faThumbsUp"
+</script>
+
+<div>Hey <Icon data={faThumbsUp} /></div>
